c7136fee18432bd7175fcada95e2304f408167f8
schliker1987 <83181475+schliker1987@users.noreply.github.com>
2021-12-14 01:13:21 -0600

    Update urls.py

--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,3 +14,5 @@ urlpatterns = [
     path('timeline/', include('userprofile.urls')),
     path('messages/', include('communications.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
