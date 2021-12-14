0594d85e45e4db4621180dd06d1c3f25f55dae74
Alexander Schliker <83181475+schliker1987@users.noreply.github.com>
2021-12-14 04:18:02 -0600

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,18 +1,23 @@
 from django.contrib import admin
 import models
+from django.contrib.auth.models import User
+
 # Register your models here.
 
-admin.site.register(User)
+#admin.site.register(User)
 
-def has_change_permission(self, request, obj=None):
-    return True
+@admin.register(User)
+class User(admin.ModelAdmin):
 
-def has_delete_permission(self, request, obj=None):
-    return False
+    def has_change_permission(self, request, obj=None):
+        return True
 
-# to disable view and add you can do this 
-def has_view_permission(self, request, obj=None):
-    return True
+    def has_delete_permission(self, request, obj=None):
+        return False
 
-def has_add_permission(self, request):
-    return True
+    # to disable view and add you can do this 
+    def has_view_permission(self, request, obj=None):
+        return True
+
+    def has_add_permission(self, request):
+        return True
