cf11fd2a56841493df0c0254c96aba83edef0f31
Alexander Schliker <83181475+schliker1987@users.noreply.github.com>
2021-12-14 04:03:09 -0600

    Update settings.py

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -58,12 +58,12 @@ WSGI_APPLICATION = 'socialnetwork.wsgi.application'
 ASGI_APPLICATION = "socialnetwork.routing.application"
 
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
+    #'default': {
+    #    'BACKEND': 'channels_redis.core.RedisChannelLayer',
+    #    'CONFIG': {
+    #        "hosts": [('127.0.0.1', 6379)],
+    #    },
+    #},
 }
 
 DATABASES = {
