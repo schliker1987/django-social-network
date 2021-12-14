96bd59e94555b6c304415d3d4d36f4cef65e1ec7
Alexander Schliker <83181475+schliker1987@users.noreply.github.com>
2021-12-14 10:13:18 -0600

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Register your models here.
 
-#admin.site.register(User)
+admin.site.register(User)
 
 @admin.register(User)
 class User(admin.ModelAdmin):
