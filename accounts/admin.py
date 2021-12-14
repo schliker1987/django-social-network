a93abbd96a7b3278abd7842c4b31c5bdb256ad73
Alexander Schliker <83181475+schliker1987@users.noreply.github.com>
2021-12-14 04:14:19 -0600

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,3 +3,16 @@ import models
 # Register your models here.
 
 admin.site.register(User)
+
+def has_change_permission(self, request, obj=None):
+    return True
+
+def has_delete_permission(self, request, obj=None):
+    return False
+
+# to disable view and add you can do this 
+def has_view_permission(self, request, obj=None):
+    return True
+
+def has_add_permission(self, request):
+    return True
